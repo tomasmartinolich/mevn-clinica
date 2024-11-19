cc17b9ce024fd340863182c22b04a18c2c20cf97
Tomás Martinolich <tomasmartinolich@gmail.com>
2024-11-19 09:42:55 -0300

    agregar hoja pacientes

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ io.on('connection', function (socket) {
 })
 
 //mongoose.connect("mongodb://localhost/mevn-database")
-mongoose.connect(process.env.DB_URL)
+mongoose.connect('mongodb://localhost/mevn-local')
     .then(db => console.log("DB is connected."))
     .catch(err => console.error(err));
 
 //Settings
-//app.set('port', process.env.PORT || 3000);      // process.env.PORT sirve para que tome el puerto que te da el sist. operativo. Si no hay, que use 3000
+app.set('port', process.env.PORT || 3000);      // process.env.PORT sirve para que tome el puerto que te da el sist. operativo. Si no hay, que use 3000
 
 //Middlewares                   (funciones que se ejecutan antes de que entren a las rutas)
 app.use(morgan('dev'));         //cuando recibas peticiones del navegador en el servidor, el servidor te pueda mostrar qué peticiones ha pedido el navegador
@@ -46,6 +46,9 @@ app.use(express.static(__dirname + '/public'));         // __dirname te tira la 
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-server.listen(port, '0.0.0.0', () =>{
+console.log(host);
+console.log(port);
+
+server.listen(port, host, () =>{
     console.log("El servidor está funcionando");
 })
